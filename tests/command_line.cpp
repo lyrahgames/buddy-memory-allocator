4d8382cab0f2e88089732e468aaa86c546ceae7e
lyrahgames <markus.pawellek@mailbox.org>
2020-06-17 16:13:41 +0200

    Add random routine to command line example

--- tests/command_line.cpp
+++ tests/command_line.cpp
@@ -25,13 +25,8 @@ int main(int argc, char* argv[]) {
   input >> size;
 
   mt19937 rng{random_device{}()};
-  uniform_real_distribution<float> alloc_or_free_dist{};
-
   unordered_map<void*, size_t> allocation_table{};
-
   buddy_system::arena arena{size};
-
-  cout << '\n' << arena << '\n';
 
   prompt();
   string cmd;
@@ -39,9 +34,10 @@ int main(int argc, char* argv[]) {
   while (cin >> cmd) {
     getline(std::cin, arg);
 
+    cout << '\n';
+    separate();
+
     if (cmd == "help") {
-      cout << '\n';
-      separate();
       cout << "Help Message\n";
       separate();
       cout << setw(20) << "help" << setfill('.') << setw(10) << '.'
@@ -57,31 +53,26 @@ int main(int argc, char* argv[]) {
            << '\n'
            << setw(20) << "free <index>" << setfill('.') << setw(10) << '.'
            << setfill(' ') << "Deallocate memory block with index 'index'."
+           << '\n'
+           << setw(20) << "random" << setfill('.') << setw(10) << '.'
+           << setfill(' ') << "Randomly allocate or deallocate memory blocks."
            << '\n';
-      separate();
-      cout << '\n';
     } else if (cmd == "q" || cmd == "quit")
       break;
     else if (cmd == "arena") {
       cout << '\n' << arena << '\n';
     } else if (cmd == "list") {
-      cout << '\n';
-      separate();
       cout << setw(20) << "address" << setw(15) << "index" << setw(20)
            << "size [B]" << setw(20) << "page size [B]" << '\n';
       separate();
       for (auto [p, n] : allocation_table)
         cout << setw(20) << p << setw(15) << arena.index_of_node_ptr(p)
              << setw(20) << n << setw(20) << arena.page_size(p) << '\n';
-      separate();
-      cout << '\n';
     } else if (cmd == "malloc") {
       size_t size;
       stringstream tmp{arg};
       tmp >> size;
       const auto p = arena.malloc(size);
-      cout << '\n';
-      separate();
       if (!p) {
         cout << "Memory allocation with " << size << " B was unsuccessful.\n"
              << "No remaining free page is large enough.\n";
@@ -96,14 +87,10 @@ int main(int argc, char* argv[]) {
              << setw(20) << "alignment = " << buddy_system::alignment_of_ptr(p)
              << " B" << '\n';
       }
-      separate();
-      cout << '\n';
     } else if (cmd == "free") {
       size_t index;
       stringstream tmp{arg};
       tmp >> index;
-      cout << '\n';
-      separate();
       const auto p = arena.void_ptr_of_index(index);
       const auto it = allocation_table.find(p);
       if (it != allocation_table.end()) {
@@ -114,47 +101,52 @@ int main(int argc, char* argv[]) {
         cout << "Cannot deallocate memory block with given index " << index
              << ".\n";
       }
-      separate();
-      cout << '\n';
+    } else if (cmd == "random") {
+      if ((allocation_table.size() == 0) ||
+          uniform_real_distribution<float>{}(rng) < 0.6f) {
+        uniform_int_distribution<size_t> mem_exp_dist{
+            0, buddy_system::log2(arena.max_page_size() - 1) + 1};
+        const auto mem_exp = mem_exp_dist(rng);
+        auto mem_size = size_t{1} << mem_exp;
+        mem_size += uniform_int_distribution<size_t>{0, mem_size - 1}(rng);
+        cout << "Randomly allocate memory of size " << mem_size << " B:\n";
+        const auto p = arena.malloc(mem_size);
+        if (!p) {
+          cout << "Memory allocation with " << mem_size
+               << " B was unsuccessful.\n"
+               << "No remaining free page is large enough.\n";
+        } else {
+          allocation_table[p] = mem_size;
+          cout << "Memory allocation with size " << mem_size
+               << " B was successful.\n\n"
+               << setw(20) << "address = " << p << '\n'
+               << setw(20) << "index = " << arena.index_of_node_ptr(p) << '\n'
+               << setw(20) << "size = " << mem_size << " B" << '\n'
+               << setw(20) << "page size = " << arena.page_size(p) << " B"
+               << '\n'
+               << setw(20)
+               << "alignment = " << buddy_system::alignment_of_ptr(p) << " B"
+               << '\n';
+        }
+      } else {
+        uniform_int_distribution<size_t> pointer_dist{
+            0, allocation_table.size() - 1};
+        const auto index = pointer_dist(rng);
+        auto it = allocation_table.begin();
+        advance(it, index);
+        cout << "Randomly deallocate memory block with index "
+             << arena.index_of_node_ptr(it->first) << " (" << it->first << ")"
+             << '\n';
+        arena.free(it->first);
+        allocation_table.erase(it);
+      }
     } else {
       cout << "\nUnknown command. Use 'help' to print the help message.\n\n";
     }
 
-    //   if ((allocation_table.size() == 0) ||
-    //       alloc_or_free_dist(rng) < 0.6f) {
-    //     uniform_int_distribution<int> mem_exp_dist{4, 31};
-    //     const auto mem_exp = mem_exp_dist(rng);
-    //     const auto mem_size = (size_t{1} << mem_exp) - 8;
+    separate();
+    cout << '\n';
 
-    //     cout << "allocate " << mem_size << " B:\n";
-
-    //     const auto p = arena.malloc(mem_size);
-    //     if (!p) {
-    //       cout << "unsuccessful\n";
-    //     } else {
-    //       allocation_table.insert(p);
-    //       cout << "success: p = " << arena.index_of_node_ptr(p) << " (" << p
-    //            << ")" << '\n'
-    //            << "page size = " << arena.page_size(p) << '\n'
-    //            << "alignment = " << buddy_system::alignment_of_ptr(p) <<
-    //            '\n';
-    //     }
-    //   } else {
-    //     uniform_int_distribution<size_t> pointer_dist{
-    //         0, allocation_table.size() - 1};
-    //     const auto index = pointer_dist(rng);
-
-    //     auto it = allocation_table.begin();
-    //     advance(it, index);
-
-    //     cout << "deallocate " << arena.index_of_node_ptr(*it) << " (" << *it
-    //          << ")" << '\n';
-
-    //     arena.free(*it);
-    //     allocation_table.erase(it);
-    //   }
-
-    //   cout << arena;
     prompt();
   }
 }
